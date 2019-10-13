fa7d71d011205bef14afaf39d7cacef81ffae398
Jairo Duarte <alfredojairo17@hotmail.com>
2019-10-13 15:21:42 +0100

    Feature setup ci (#5)
    * add github actions CI/CD

--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -20,6 +20,7 @@ export default function Offer({ match: { params } }) {
 
         setPictures(result.data);
       } catch (error) {
+        // eslint-disable-next-line no-console
         console.log(error);
       }
     }
